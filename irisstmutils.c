09d6486654fe25118f7d48094f6708f08e949fb4
Andika R. N. Nugraha <107173533+arnn5115@users.noreply.github.com>
2025-01-13 21:19:17 +0700

    Tambah file library

---
+++ irisstmutils.c
@@ -0,0 +1,156 @@
+/*
+ * irisstmutils.c
+ *
+ *  Created on: Dec 19, 2024
+ *      Author: ARNN5115
+ *     Version: 1.0
+ */
+
+#include "irisstmutils.h"
+
+uint32_t millis(void) {
+    return HAL_GetTick();
+}
+
+uint32_t micros(void) {
+    return HAL_GetTick() * 1000 + ((SysTick->LOAD - SysTick->VAL) * 1000) / HAL_RCC_GetHCLKFreq();
+}
+
+void delay_us(uint32_t us) {
+    uint32_t start = micros();
+    while (micros() - start < us);
+}
+
+uint32_t map(uint32_t x, uint32_t in_min, uint32_t in_max, uint32_t out_min, uint32_t out_max) {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+uint32_t mapf(float x, float in_min, float in_max, float out_min, float out_max) {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+uint32_t constrain(uint32_t x, uint32_t min, uint32_t max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+}
+
+RingBuffer* ringBufferInit(uint16_t size) {
+    RingBuffer* rb = (RingBuffer*)malloc(sizeof(RingBuffer));
+    if (!rb) return NULL;
+    
+    rb->buffer = (uint8_t*)malloc(size);
+    if (!rb->buffer) {
+        free(rb);
+        return NULL;
+    }
+    
+    rb->size = size;
+    rb->head = 0;
+    rb->tail = 0;
+    return rb;
+}
+
+int ringBufferPush(RingBuffer* rb, uint8_t data) {
+    if (!rb) return -1;
+    
+    uint16_t next = (rb->head + 1) % rb->size;
+    if (next == rb->tail) return -1;
+    
+    rb->buffer[rb->head] = data;
+    rb->head = next;
+    return 0;
+}
+
+int ringBufferPop(RingBuffer* rb, uint8_t* data) {
+    if (!rb || rb->head == rb->tail) return -1;
+    
+    *data = rb->buffer[rb->tail];
+    rb->tail = (rb->tail + 1) % rb->size;
+    return 0;
+}
+
+int setPWMDuty16bit(TIM_HandleTypeDef *tim, uint8_t channel, uint16_t duty)
+{
+    uint32_t period = tim->Init.Period + 1;
+    uint32_t scaled_duty = (uint32_t)duty * period;
+    uint32_t pulse = scaled_duty >> 16;
+
+    uint64_t tim_channel;
+    switch (channel)
+    {
+    case 1:
+        tim_channel = TIM_CHANNEL_1;
+        break;
+
+    case 2:
+        tim_channel = TIM_CHANNEL_2;
+        break;
+
+    case 3:
+        tim_channel = TIM_CHANNEL_3;
+        break;
+    
+    case 4:
+        tim_channel = TIM_CHANNEL_4;
+        break;
+    
+    default:
+        Error_Handler();
+    }
+
+    __HAL_TIM_SET_COMPARE(tim, tim_channel, pulse);
+}
+
+uint16_t power(uint16_t base, uint16_t exp){
+    if(exp == 0){
+        return 1;
+    } else {
+        return base * power(base, exp - 1);
+    }
+}
+
+int sortUART_RXData(const char *unsorted_array, char *header, char *sorted_array){
+    // Buat pointer yang menunjuk ke elemen pertama header (i)
+    uint8_t *id = strstr(unsorted_array, header);
+    // Cek jika pointer NULL -> header tidak ditemukan
+    if (id == NULL){
+        return -1;
+    } // Urutkan data JIKA DAN HANYA JIKA header ditemukan
+    uint8_t len = sizeof(unsorted_array);
+    for (size_t i = 0; i < len; i++, id++){
+        // Handel overflow
+        if (id >= (uint8_t *)(unsorted_array+len)) 
+            id -= len; 
+        sorted_array[i] = *id;
+    }
+    return 0;
+}
+
+// uint8_t processButton8bit(char btn_list[8][3]){
+//     uint8_t button_flag = 0;
+//     uint64_t button_letters[8];
+//     uint64_t button_numbers[8];
+//     for (int i = 0; i < 8; i++){
+//         if (btn_list[i] == 'A'){
+//             button_letters[i] = GPIOA;
+//         } else if (btn_list[i] == 'B'){
+//             button_letters[i] = GPIOB;
+//         } else if (btn_list[i] == 'C'){
+//             button_letters[i] = GPIOC;
+//         } else if (btn_list[i] == 'D'){
+//             button_letters[i] = GPIOD;
+//         } else if (btn_list[i] == 'E'){
+//             button_letters[i] = GPIOE;
+//         } else {
+//             Error_Handler();
+//         }
+
+//         uint8_t button_number = (btn_list[i][1] - '0')*10 + btn_list[i][2];
+
+//         if (HAL_GPIO_ReadPin(button_letters[i], button_numbers[i]) == GPIO_PIN_SET){
+//             button_flag |= power(2, i);
+//         }
+//     }
+//   return button_flag;
+// }
---
+++ irisstmutils.h
@@ -0,0 +1,151 @@
+/*
+ * irisstmutils.h
+ *
+ *  Created on: Dec 19, 2024
+ *      Author: ARNN5115
+ *     Version: 1.0
+ * 
+ *  Just some utility functions for STM32.
+ *  
+ *  Made for ITS's IRIS Robotics Team purpose.
+ * 	Team Website: https://iris.its.ac.id.
+ */
+
+#ifndef IRIS_STM_UTILS_H
+#define IRIS_STM_UTILS_H
+
+#include "main.h"
+#include "core_cm3.h"
+
+#include <stdio.h>
+#include <stdint.h>
+
+/**
+ * @brief Structure representing a ring buffer.
+ */
+typedef struct {
+    uint8_t *buffer;
+    uint32_t head;
+    uint32_t tail;
+    uint32_t size;
+} RingBuffer;
+
+/**
+ * @brief Get the number of milliseconds since the system started.
+ * 
+ * @return Number of milliseconds since the system started.
+ * @note This function is trying to imitate the Arduino millis function.
+ */
+uint32_t millis(void);
+
+/**
+ * @brief Get the number of microseconds since the system started.
+ * 
+ * @return Number of microseconds since the system started.
+ * @note This function is trying to imitate the Arduino micros function.
+ */
+uint32_t micros(void);
+
+/**
+ * @brief Delay the program for a number of milliseconds.
+ * 
+ * @param ms Number of milliseconds to delay.
+ * @note This function is trying to imitate the Arduino delay function.
+ */
+void delay_us(uint32_t us);
+
+/**
+ * @brief Map a value from one range to another.
+ * 
+ * @param x Value to map.
+ * @param in_min Minimum value of the input range.
+ * @param in_max Maximum value of the input range.
+ * @param out_min Minimum value of the output range.
+ * @param out_max Maximum value of the output range.
+ * @return Mapped value.
+ * @note This function is trying to imitate the Arduino map function.
+ * @see https://www.arduino.cc/reference/en/language/functions/math/map/
+ */
+uint32_t map(uint32_t x, uint32_t in_min, uint32_t in_max, uint32_t out_min, uint32_t out_max);
+
+/**
+ * @brief Map a value from one range to another.
+ * 
+ * @param x Value to map.
+ * @param in_min Minimum value of the input range.
+ * @param in_max Maximum value of the input range.
+ * @param out_min Minimum value of the output range.
+ * @param out_max Maximum value of the output range.
+ * @return Mapped value.
+ * @note This function is trying to imitate the Arduino map function.
+ * @see https://www.arduino.cc/reference/en/language/functions/math/map/
+ */
+uint32_t mapf(float x, float in_min, float in_max, float out_min, float out_max);
+
+/**
+ * @brief Constrain a value to a range.
+ * 
+ * @param x Value to constrain.
+ * @param min Minimum value of the range.
+ * @param max Maximum value of the range.
+ * @return Constrained value.
+ * @note This function is trying to imitate the Arduino constrain function.
+ * @see https://www.arduino.cc/reference/en/language/functions/math/constrain/
+ */
+uint32_t constrain(uint32_t x, uint32_t min, uint32_t max);
+
+/**
+ * @brief Initialize a ring buffer.
+ * 
+ * @param size Size of the ring buffer.
+ * @return Pointer to the initialized ring buffer.
+ */
+RingBuffer* ringBufferInit(uint16_t size);
+
+/**
+ * @brief Push a byte to the ring buffer.
+ * 
+ * @param rb Pointer to the ring buffer.
+ * @param data Data to push.
+ * @return 0 on success, -1 on failure.
+ */
+int ringBufferPush(RingBuffer* rb, uint8_t data);
+
+/**
+ * @brief Pop a byte from the ring buffer.
+ * 
+ * @param rb Pointer to the ring buffer.
+ * @param data Pointer to the data to pop.
+ * @return 0 on success, -1 on failure.
+ */
+int ringBufferPop(RingBuffer* rb, uint8_t* data);
+
+/**
+ * @brief Set the PWM duty cycle from a 16-bit input.
+ * 
+ * @param tim Timer handler.
+ * @param channel PWM channel.
+ * @param duty Duty cycle in 16-bit resolution.
+ * @return 0 on success, -1 on failure.
+ */
+int setPWMDuty16bit(TIM_HandleTypeDef *tim, uint8_t channel, uint16_t duty);
+
+/**
+ * @brief Sort UART RX data into an array.
+ * 
+ * @param unsorted_array Pointer to the unsorted input array.
+ * @param header Pointer to store header characters.
+ * @param sorted_array Pointer to store sorted output array.
+ * @return 0 on success, -1 on failure.
+ */
+int sortUART_RXData(const char *unsorted_array, char *header, char *sorted_array);
+
+// /**
+//  * @brief Process 8-bit buttons state from a list of button names.
+//  * 
+//  * @param btn_list Array of 8 button names, e.g: "E01"
+//  * @return Combined 8-bit value representing button states
+//  */
+// uint8_t processButton8bit(char btn_list[8][3]);
+
+#endif /* IRIS_STM_UTILS_H */
